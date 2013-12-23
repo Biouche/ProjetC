dae2bffd4956c25eeb4bc2c2cb3acdac0a24c240
dev <dev@ubuntu>
2013-12-23 18:11:30 +0100

    ok

--- ProjetC/src/FileManip.c
+++ ProjetC/src/FileManip.c
@@ -6,3 +6,22 @@
  */
 
 
+#include <stdio.h>
+#include <string.h>
+#include "FileManip.h"
+#include "Constants.h"
+
+int CreateTable (char* request)
+{
+	char nomTable [TAILLE_NOM_TABLE];
+	FILE ficTable;
+
+	strcat(request,".txt");
+	//Test de l'existence du fichier
+	ficTable = fopen (request,'r');
+	if ( ficTable == NULL)
+			return -1;
+
+
+
+}
