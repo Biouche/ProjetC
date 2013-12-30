48bd05ab17b34249816fb9160564f040a9ce3be6
dev <dev@ubuntu>
2013-12-30 18:17:25 +0100

    tt

--- ProjetC/src/IHM.c
+++ ProjetC/src/IHM.c
@@ -15,6 +15,7 @@
 
 void IHM(void)
 {
+	int errorCode=0;
 	char request[TAILLE_REQUEST];
 	memset(request,0,sizeof(char)*TAILLE_REQUEST);
 	gets(request);
@@ -29,14 +30,15 @@ void IHM(void)
 		buffer=strtok(NULL," ");
 		if(strcmp(buffer,"TABLE")==0)
 		{
-		buffer=strtok(NULL," ");
+			buffer=strtok(NULL," ");
+			errorCode=CreateTable(buffer);
+			PrintError(errorCode,1);
 		}
 		else
 		{
 			printf("Erreur de saisie. Cf HELP");
 		}
 		//recuperation du nom de la table dans le buffer
-		CreateTable(buffer);
 	}
 	else if(strcmp(buffer,"DROP")==0)
 	{
@@ -44,13 +46,13 @@ void IHM(void)
 		if(strcmp(buffer,"TABLE")==0)
 		{
 		buffer=strtok(NULL," ");
+		errorCode=DropTable(buffer);
 		}
 		else
 		{
 			printf("Erreur de saisie. Cf HELP");
 		}
 		//recuperation du nom de la table dans le buffer
-		//DropTable(buffer);
 	}
 	else if(strcmp(buffer,"SELECT")==0)
 	{
@@ -69,7 +71,7 @@ void IHM(void)
 		buffer=strtok(NULL," ");
 		strcat(requestSelect,buffer);
 		//printf("requestSelect:<%s>\n",requestSelect);
-		//Select(resquetSelect);
+		errorCode=Select(resquetSelect);
 	}
 	else if(strcmp(buffer,"INSERT")==0)
 	{
@@ -83,7 +85,7 @@ void IHM(void)
 		printf("Erreur de saisie. Cf HELP");
 		}
 		//recuperation du nom de la table dans le buffer
-		//InsertInto(buffer);
+		errorCode=InsertInto(buffer);
 	}
 	else if(strcmp(buffer,"DELETE")==0)
 	{
@@ -101,7 +103,7 @@ void IHM(void)
 		}
 		buffer=strtok(NULL," ");
 		strcat(requestSelect,buffer);
-		//Delete(resquetSelect);
+		errorCode=Delete(resquetSelect);
 	}
 	else if(strcmp(buffer,"INDEX")==0)
 	{
@@ -126,6 +128,7 @@ void IHM(void)
 		strcat(requestSelect," ");
 		buffer=strtok(NULL," ");
 		strcat(requestSelect,buffer);
+		errorCode=Update(requestSelect);
 	}
 	else if(strcmp(buffer,"ALTER")==0)
 	{
@@ -146,6 +149,7 @@ void IHM(void)
 			}
 			buffer=strtok(NULL," ");
 			strcat(requestSelect,buffer);
+			errorCode=AlterTable(requestSelect);
 			//printf("requestSelect:<%s>\n",requestSelect);
 		}
 		else
@@ -165,10 +169,11 @@ void IHM(void)
 void Help(void)
 {
 	int i=0;
-	while(1)
+	int quitter=0;
+	do
 	{
-		printf("************Help Menu************\n");
-		printf("\tRequest Description:\n");
+		printf("************Help Menu************\n\n");
+		printf("\tRequest Description:\n\n");
 		printf("\t(0) CREATE TABLE\n");
 		printf("\t(1) DROP TABLE\n");
 		printf("\t(2) ALTER TABLE\n");
@@ -177,58 +182,76 @@ void Help(void)
 		printf("\t(5) UPDATE\n");
 		printf("\t(6) CREATE INDEX\n");
 		printf("\t(7) DELETE\n");
-		printf("\t(8) Quitter HELP\n");
+		printf("\t(8) Quitter HELP\n\n\n");
 		scanf("%d",&i);
 		switch(i)
 		{
-			case 0:printf("*********CREATE TABLE Description*********\n");
-				   printf("\Cette request permet la création d'une table\n");
-				   printf("*********CREATE TABLE Syntax*********\n");
-				   printf("\tExemple : CREATE TABLE <NomTable>\n");
+			case 0:printf("*********CREATE TABLE Description*********\n\n");
+				   printf("\tCette request permet la création d'une table\n\n\n");
+				   printf("*********CREATE TABLE Syntax*********\n\n");
+				   printf("\tExemple : CREATE TABLE <NomTable>\n\n\n");
 				   break;
-			case 1:printf("*********DROP TABLE Description*********\n");
-			   	   printf("\tCette request permet la suppression d'une table\n");
-			   	   printf("*********DROP TABLE Syntax*********\n");
-			   	   printf("\tExemple : DROP TABLE <NomTable>\n");
+			case 1:printf("*********DROP TABLE Description*********\n\n");
+			   	   printf("\tCette request permet la suppression d'une table\n\n\n");
+			   	   printf("*********DROP TABLE Syntax*********\n\n");
+			   	   printf("\tExemple : DROP TABLE <NomTable>\n\n\n");
 			   	   break;
-			case 2:printf("*********ALTER TABLE Description*********\n");
-			   	   printf("\tCette request permet l'ajout d'une colonne via ADD COLUMN au sein d'une table existante\n");
-			   	   printf("*********ALTER TABLE Syntax*********\n");
-			   	   printf("\tExemple : ALTER TABLE <NomTable> ADD COLUMN <NomColonne> <Type>\n");
+			case 2:printf("*********ALTER TABLE Description*********\n\n");
+			   	   printf("\tCette request permet l'ajout d'une colonne via ADD COLUMN au sein d'une table existante\n\n\n");
+			   	   printf("*********ALTER TABLE Syntax*********\n\n");
+			   	   printf("\tExemple : ALTER TABLE <NomTable> ADD COLUMN <NomColonne> <Type>\n\n\n");
 			   	   break;
-			case 3:printf("*********INSERT INTO Description*********\n");
-			   	   printf("\tCette request permet l'ajout d'un enregistrement au sein d'une table existante\n");
-			   	   printf("*********INSERT INTO Syntax*********\n");
-			   	   printf("\tExemple : INSERT INTO <NomTable> VALUES <ValeursColonne1> <ValeursColonne2>\n");
+			case 3:printf("*********INSERT INTO Description*********\n\n");
+			   	   printf("\tCette request permet l'ajout d'un enregistrement au sein d'une table existante\n\n\n");
+			   	   printf("*********INSERT INTO Syntax*********\n\n");
+			   	   printf("\tExemple : INSERT INTO <NomTable> VALUES <ValeursColonne1> <ValeursColonne2>\n\n\n");
 			   	   break;
-			case 4:printf("*********SELECT Description*********\n");
-			   	   printf("\tCette request permet la selection partielle ou totale d'une table et l'affiche suivant le souhait de l'utilisateur\n");
-			   	   printf("*********SELECT Syntax*********\n");
-			   	   printf("\tExemple : SELECT WHERE <NomColonne> = <Valeur> FROM <NomTable>\n");
+			case 4:printf("*********SELECT Description*********\n\n");
+			   	   printf("\tCette request permet la selection partielle ou totale d'une table et l'affiche suivant le souhait de l'utilisateur\n\n\n");
+			   	   printf("*********SELECT Syntax*********\n\n");
+			   	   printf("\tExemple : SELECT WHERE <NomColonne> = <Valeur> FROM <NomTable>\n\n\n");
 			   	   break;
-			case 5:printf("*********UPDATE Description*********\n");
-				   printf("\tCette request permet la modification d'un ou plusieurs enregistrements suivant une condition définie par l'utilisateur\n");
-				   printf("*********UPDATE Syntax*********\n");
-				   printf("\tExemple : UPDATE <NomTable> SET <NomColonne1> = <Valeur1> <NomColonne2> = <Valeur2> WHERE <NomColonne> = <Valeur>\n");
+			case 5:printf("*********UPDATE Description*********\n\n");
+				   printf("\tCette request permet la modification d'un ou plusieurs enregistrements suivant une condition définie par l'utilisateur\n\n\n");
+				   printf("*********UPDATE Syntax*********\n\n");
+				   printf("\tExemple : UPDATE <NomTable> SET <NomColonne1> = <Valeur1> <NomColonne2> = <Valeur2> WHERE <NomColonne> = <Valeur>\n\n\n");
 				   break;
-			case 6:printf("*********CREATE INDEX Description*********\n");
-			   	   printf("\t\n");
-			   	   printf("*********CREATE INDEX Syntax*********\n");
-			       printf("\t\n");
+			case 6:printf("*********CREATE INDEX Description*********\n\n");
+			   	   printf("\t\n\n\n");
+			   	   printf("*********CREATE INDEX Syntax*********\n\n");
+			       printf("\t\n\n\n");
 			   	   break;
-			case 7:printf("*********DELETE Description*********\n");
-			   	   printf("\tCette request permet la suppression d'un ou plusieurs enregistrement suivant une condition\n");
-			   	   printf("*********DELETE Syntax*********\n");
-			   	   printf("\tExemple : DELETE FROM <NomTable WHERE <NomColonne> = <Valeur>\n");
+			case 7:printf("*********DELETE Description*********\n\n");
+			   	   printf("\tCette request permet la suppression d'un ou plusieurs enregistrement suivant une condition\n\n\n");
+			   	   printf("*********DELETE Syntax*********\n\n");
+			   	   printf("\tExemple : DELETE FROM <NomTable WHERE <NomColonne> = <Valeur>\n\n\n");
 			   	   break;
-			case 8:IHM();
+			case 8:quitter=1;
+				   break;
 			default:printf("Vous devez saisir un chiffre entre 0 et 8.");
 		}
-	}
+
+	}while(quitter!=1);
+	getchar();
+	IHM();
 }
 
-void PrintError(int errorCode)
+void PrintError(int errorCode,int fonction)
 {
+	int errorCode=0;
+	int fonction=0;
+	if(fonction==1)
+	{
+		switch(errorCode)
+		{
+			case 0:
+			case 1:
+			case-1:
+		}
+	}
+
+
+
 
 }
 
